a861d0855103c36f9d62752bea8acacd3b6db66f
Jecjune <jecjune@qq.com>
2024-10-20 21:30:26 +0800

    trans finished

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,32 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+
+        let parts: Vec<&str> = s.split(',').collect();
+
+        if parts.is_empty() || parts[0].is_empty() {
+            return Person::default();
+        }
+
+        let name = parts[0].to_string();
+
+        if name.is_empty() || parts.len() > 2 {
+            return Person::default();
+        }
+
+        if parts.len() == 2 {
+            if let Ok(age) = parts[1].parse::<usize>() {
+                return Person { name, age };
+            }
+        }
+
+        Person::default()
     }
 }
 
